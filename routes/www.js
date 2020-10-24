7ae1d320162e2b73628bbdd71e25b73495e1d143
Fundalytica <fotios@fundalytica.com>
2020-10-24 16:06:36 +0000

    routes cleanup

--- routes/www.js
+++ routes/www.js
@@ -1,36 +1,46 @@
 var express = require('express')
 var router = express.Router()
 
+const description = 'Searching for the best performing assets and building investment and trading tools.'
+
 const topics = [
-  {   id: 'dip',
-      color: '#f55a5a',
-      image: 'dip.svg',
-      title: 'S&P 500 Dip',
-      description: 'We are looking out for the next S&P 500 dip because we love investing at low prices. Do not miss it!',
-      button: 'Buy The Dip 📉',
-      action: 'route',
-      hashtag: 'tool' },
+	{
+		id: 'dip',
+		hashtag: 'tool',
+		color: 'alizarin-500',
+		image: 'dip.svg',
+		title: 'S&P 500 Dip',
+		description: 'We are looking out for the next S&P 500 dip because we love investing at low prices. Do not miss it!',
+		button: 'Buy The Dip 📉',
+		action: 'route'
+	},
 
-  {   id: 'qqq',
-      color: '#AD5D9D',
-      image: 'invesco.svg',
-      title: 'QQQ ETF Holdings',
-      description: 'All the great companies included in the Invesco QQQ ETF (NASDAQ 100).',
-      button: 'View Stock Data',
-      action: 'route',
-      hashtag: 'etf' },
+	{
+		id: 'ipodata',
+		hashtag: 'data',
+		image: 'options.svg',
+		color: 'belize-hole-300',
+		title: 'IPO Datastore',
+		description: 'We collected and enriched all IPO data of the last 10 years so you can use it too.',
+		button: 'Preview Data 👾'
+	},
 
-  {   id: 'norway',
-      color: '#409ECD',
-      image: 'norges.png',
-      title: 'Government Pension Fund Global',
-      description: 'We dive into annual reports and analyze one of the largest pension funds in the world.',
-      button: 'View Top Holdings',
-      action: 'route',
-      hashtag: 'institution' }
+	{
+		id: 'qqq',
+		hashtag: 'etf',
+		color: 'amethyst-400 ',
+		image: 'invesco.svg',
+		title: 'QQQ ETF Holdings',
+		description: 'All the great companies included in the Invesco QQQ ETF (NASDAQ 100).',
+		button: 'Coming Soon',
+		// button: 'View Stocks 🔍',
+		// action: 'route'
+	},
+]
 
-router.get('/', (req, res) => res.render('index', { topics }))
-router.get('/sc', (req, res) => res.render('index', { topics: topics, subscribe: 'confirm' })) // subscription confirm
-router.get('/ss', (req, res) => res.render('index', { topics: topics, subscribe: 'success' })) // subscription success
+router.get('/', (req, res) => res.render('index', { topics, description }))
+
+router.get('/subscription-pending', (req, res) => res.render('index', { topics: topics, subscribe: 'pending' })) // subscription pending
+router.get('/subscription-success', (req, res) => res.render('index', { topics: topics, subscribe: 'success' })) // subscription success
 
 module.exports = router
