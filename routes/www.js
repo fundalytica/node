e7db51935643ee884edf8c6a63996f02f7def902
Fundalytica <fotios@fundalytica.com>
2020-10-24 17:06:41 +0000

    spaces 4 identation

--- routes/www.js
+++ routes/www.js
@@ -4,38 +4,38 @@ var router = express.Router()
 const description = 'Searching for the best performing assets and building investment and trading tools.'
 
 const topics = [
-	{
-		id: 'dip',
-		hashtag: 'tool',
-		color: 'alizarin-500',
-		image: 'dip.svg',
-		title: 'S&P 500 Dip',
-		description: 'We are looking out for the next S&P 500 dip because we love investing at low prices. Do not miss it!',
-		button: 'Buy The Dip 📉',
-		action: 'route'
-	},
+    {
+        id: 'dip',
+        hashtag: 'tool',
+        color: 'alizarin-500',
+        image: 'dip.svg',
+        title: 'S&P 500 Dip',
+        description: 'We are looking out for the next S&P 500 dip because we love investing at low prices. Do not miss it!',
+        button: 'Buy The Dip 📉',
+        action: 'route'
+    },
 
-	{
-		id: 'ipodata',
-		hashtag: 'data',
-		image: 'options.svg',
-		color: 'belize-hole-300',
-		title: 'IPO Datastore',
-		description: 'We collected and enriched all IPO data of the last 10 years so you can use it too.',
-		button: 'Preview Data 👾'
-	},
+    {
+        id: 'ipodata',
+        hashtag: 'data',
+        image: 'options.svg',
+        color: 'belize-hole-300',
+        title: 'IPO Datastore',
+        description: 'We collected and enriched all IPO data of the last 10 years so you can use it too.',
+        button: 'Preview Data 👾'
+    },
 
-	{
-		id: 'qqq',
-		hashtag: 'etf',
-		color: 'amethyst-400 ',
-		image: 'invesco.svg',
-		title: 'QQQ ETF Holdings',
-		description: 'All the great companies included in the Invesco QQQ ETF (NASDAQ 100).',
-		button: 'Coming Soon',
-		// button: 'View Stocks 🔍',
-		// action: 'route'
-	},
+    {
+        id: 'qqq',
+        hashtag: 'etf',
+        color: 'amethyst-400 ',
+        image: 'invesco.svg',
+        title: 'QQQ ETF Holdings',
+        description: 'All the great companies included in the Invesco QQQ ETF (NASDAQ 100).',
+        button: 'Coming Soon',
+        // button: 'View Stocks 🔍',
+        // action: 'route'
+    },
 ]
 
 router.get('/', (req, res) => res.render('index', { topics, description }))
