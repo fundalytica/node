b3247f77c9dcf6e71d02abc1e5e7341674d7db5d
Fundalytica <fotios@fundalytica.com>
2020-10-24 16:45:03 +0000

    removed logger

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var wwwRouter = require('./routes/www');
-var apiRouter = require('./routes/api');
 
 var app = express();
 
@@ -13,7 +9,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -22,8 +17,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 const vhost = require('vhost')
-app.use(vhost('www.fundalytica.com', wwwRouter))
-app.use(vhost('api.fundalytica.com', apiRouter))
+app.use(vhost('www.fundalytica.com', require('./routes/www')))
+app.use(vhost('api.fundalytica.com', require('./routes/api')))
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
