9b8e82c13899b51616e85f2e5961dcfa26780270
fotios <fotios@fundalytica.com>
2020-11-01 18:08:33 +0000

    js script to set cookie if email subscription is successful and hide html element

---
+++ public/javascripts/subscription.js
@@ -0,0 +1,19 @@
+const subscription_url = document.getElementById('subscriptionjs').getAttribute('data-subscription-url')
+const subscribed_key = document.getElementById('subscriptionjs').getAttribute('data-subscribed-key')
+
+import Cookie from './modules/Cookie.js'
+
+const run = () => {
+    if(! Cookie.get(subscribed_key) && subscription_url == 'success') {
+        Cookie.set(subscribed_key)
+        $('#welcome').delay(1000 * 2).hide(1000 * 3)
+    }
+}
+
+const test = () => {
+    Cookie.reset(subscribed_key)
+    Cookie.set(subscribed_key)
+    console.log(document.cookie)
+}
+
+$(run())
