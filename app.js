48ec326cd1a9d15ff892673d4475f0ef0fdc663e
fotios <fotios@fundalytica.com>
2020-11-03 20:18:54 +0000

    add cors to allow requests to api from www

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
 
-var app = express();
+var app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+// https://expressjs.com/en/resources/middleware/cors.html
+const cors = require('cors')
+const corsOptions = { origin: 'https://www.fundalytica.com' }
+app.use(cors(corsOptions))
+
+// https://expressjs.com/en/resources/middleware/vhost.html
 const vhost = require('vhost')
 app.use(vhost('www.fundalytica.com', require('./routes/www')))
 app.use(vhost('api.fundalytica.com', require('./routes/api')))
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-});
+app.use((req, res, next) => {
+    next(createError(404))
+})
 
 // error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.locals.message = err.message
+    // error details only in development
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
+    res.render('error')
+})
 
-module.exports = app;
+module.exports = app
