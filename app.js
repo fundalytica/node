f94c92b2b8fe1471ea32f548f71af5af69dff128
fotios <fotios@fundalytica.com>
2020-11-24 18:08:56 +0000

    use connect-flash

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const createError = require('http-errors')
 const express = require('express')
 const path = require('path')
-const cookieParser = require('cookie-parser')
+const createError = require('http-errors')
 
-var app = express()
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const flash = require('connect-flash')
+
+const app = express()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.use(cookieParser())
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(cookieParser())
-
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(session({ secret: 'fundalytica', saveUninitialized: true, resave: true }))
+app.use(flash())
 
 // https://expressjs.com/en/resources/middleware/cors.html
 const cors = require('cors')
