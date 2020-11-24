0079b936722885825341f1173cb88fb182b006fe
fotios <fotios@fundalytica.com>
2020-11-24 18:14:27 +0000

    Revert "check if joinus element is availabl"
    This reverts commit 1e5bda32e79ea85333a26f395fbf04b1647c39c2.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,16 +1,14 @@
-const text = ' Do not miss any updates. Join us!'
+const description = document.getElementById('indexjs').getAttribute('data-description')
 
 const run = () => {
-    const element = document.getElementById('join-us')
+    const element = document.getElementById('description')
 
-    if(element) {
-        const typewriter = new Typewriter(element, {
-            delay: 0.05 * 1000,
-            cursor: null
-        })
+    const typewriter = new Typewriter(element, {
+        delay: 0.05 * 1000,
+        cursor: null
+    })
 
-        typewriter.typeString(text).start()
-    }
+    typewriter.typeString(description).start()
 }
 
 $(run())
