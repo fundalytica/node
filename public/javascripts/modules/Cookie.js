ca0bf0100b683038d67c6572042d142f95cec521
fotios <fotios@fundalytica.com>
2020-12-02 18:42:27 +0000

    use value parameter instead of default

--- public/javascripts/modules/Cookie.js
+++ public/javascripts/modules/Cookie.js
@@ -1,14 +1,16 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie
 
 export default class Cookie {
-    constructor() {     }
+    constructor() { }
 
     static get SEPARATOR() { return '; ' }
     static get DEFAULT_VALUE() { return 'yes' }
 
     static set(key, value) {
+        // https://webkit.org/blog/10218/full-third-party-cookie-blocking-and-more/
+        // Intelligent Tracking Prevention (ITP), 7 days
         const expiration = new Date('2100-01-01T00:00:00') // 2100
-        document.cookie = `${key}=${Cookie.DEFAULT_VALUE}${Cookie.SEPARATOR}samesite=strict${Cookie.SEPARATOR}expires=${expiration.toUTCString()}${Cookie.SEPARATOR}`
+        document.cookie = `${key}=${value}${Cookie.SEPARATOR}samesite=strict${Cookie.SEPARATOR}expires=${expiration.toUTCString()}${Cookie.SEPARATOR}`
     }
 
     static get(key) {
@@ -21,6 +23,10 @@ export default class Cookie {
     }
 
     static reset(key, value) {
-        document.cookie = `${key}=${Cookie.DEFAULT_VALUE}${Cookie.SEPARATOR}samesite=strict${Cookie.SEPARATOR}max-age=0${Cookie.SEPARATOR}`
+        document.cookie = `${key}=${value}${Cookie.SEPARATOR}samesite=strict${Cookie.SEPARATOR}max-age=0${Cookie.SEPARATOR}`
+    }
+
+    static show() {
+        console.log(document.cookie)
     }
 }
