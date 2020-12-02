d238268681e3ea65614577cd9b25f47ba7d2a114
fotios <fotios@fundalytica.com>
2020-12-02 18:46:57 +0000

    refactor

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,22 @@
-const text = ' Do not miss any updates. Join us!'
+import Subscription from './modules/Subscription.js'
 
 const run = () => {
-    const element = document.getElementById('join-us')
+    $('.topic').show()
 
-    if(element) {
-        const typewriter = new Typewriter(element, {
-            delay: 0.05 * 1000,
-            cursor: null
-        })
+    const subscription = new Subscription('#indexjs', '#welcome')
+    // subscription.hide()
+    subscription.success()
 
+    // if(! $('#welcome').is(":hidden")) {
+    //     typewrite('Do not miss any updates. Join now!', 'join')
+    // }
+}
+
+const typewrite = (text, id) => {
+    const textElement = document.getElementById(id)
+
+    if(textElement) {
+        const typewriter = new Typewriter(textElement, { delay: 0.05 * 1000, cursor: null })
         typewriter.typeString(text).start()
     }
 }
