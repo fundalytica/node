77798f899949497c03b44debfff4ab495eee6886
fotios <fotios@fundalytica.com>
2020-12-04 15:26:03 +0000

    refactor code

--- app.js
+++ app.js
@@ -1,59 +1,52 @@
+require('dotenv').config()
+
 const express = require('express')
 const path = require('path')
-const createError = require('http-errors')
 
-const session = require('express-session')
-const MemoryStore = require('memorystore')(session)
-
-const cookieParser = require('cookie-parser')
+const session = require('express-session')          // http://expressjs.com/en/resources/middleware/session.html
+const MemoryStore = require('memorystore')(session) // https://www.npmjs.com/package/memorystore
 const flash = require('connect-flash')
+
+const vhost = require('vhost')                      // https://expressjs.com/en/resources/middleware/vhost.html
+const cors = require('cors')                        // https://expressjs.com/en/resources/middleware/cors.html
 
 const app = express()
 
-app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
-
-app.use(cookieParser())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-// http://expressjs.com/en/resources/middleware/session.html
-// https://www.npmjs.com/package/memorystore
 app.use(session({
-    cookie: { maxAge: 86400000 },                       // 24h
-    store: new MemoryStore({ checkPeriod: 86400000 }),  // 24h check period
-    secret: 'Ph3nrta6W4BfDp7MRkhR',                     // random secret
+    cookie: { sameSite: 'strict', maxAge: 86400000 },
+    store: new MemoryStore({ checkPeriod: 86400000 }),
+    secret: process.env.SESSION_SECRET,
     saveUninitialized: false,
-    resave: false,
-    cookie: { sameSite: 'strict' }
+    resave: false
 }))
 app.use(flash())
 
-// https://expressjs.com/en/resources/middleware/cors.html
-const cors = require('cors')
-const corsOptions = { origin: 'https://www.fundalytica.com' }
-app.use(cors(corsOptions))
-
-// https://expressjs.com/en/resources/middleware/vhost.html
-const vhost = require('vhost')
 app.use(vhost('www.fundalytica.com', require('./routes/www')))
+app.use(cors({ origin: 'https://www.fundalytica.com' }))
 app.use(vhost('api.fundalytica.com', require('./routes/api')))
 
-// catch 404 and forward to error handler
+// 404 handler
 app.use((req, res, next) => {
-    next(createError(404))
+    const errors = require('http-errors')
+    next(errors(404))
 })
 
 // error handler
-app.use((err, req, res, next) => {
-    res.status(err.status || 500)
-    res.locals.message = err.message
-    // error details only in development
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.locals.message = error.message
+    res.locals.error = (process.env.NODE_ENV === 'development') ? error : {}
     res.render('error')
 })
+
+app.locals.description = 'Searching for the best performing assets and building great investing tools.'
+app.locals.subscribed_key = 'subscribed'
 
 module.exports = app
