6d4116cc503d07d31daa89787fd34227f80aa2d5
fotios <fotios@fundalytica.com>
2021-05-14 14:58:15 +0000

    use shell handler for all api calls

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-const { PythonShell } = require('python-shell')
+const utils = require('./api/utils.js')
 
 router.get('/', (req, res, next) => {
     res.render('api', {
         text: 'Fundalytica API',
+
         urls: [
             '/v1/options/portfolio',
 
@@ -13,15 +14,11 @@ router.get('/', (req, res, next) => {
 
             '/v1/historical/:symbol',
             '/v1/historical/ath/:symbol',
-            '/v1/historical/dip/:symbol-:dip'
+            '/v1/historical/dip/:symbol-:dip',
+
+            '/v1/crypto/futures/tickers/symbols/kraken',
         ]
     })
-})
-
-router.get('/v1/options/portfolio', async (req, res) => {
-    console.log('/v1/options/portfolio')
-
-    shellHandler('/scripts/ib-summary/ib-summary.py', ['--options', '--json'], res)
 })
 
 router.get('/v1/quote/:symbol', async (req, res) => {
@@ -29,8 +26,8 @@ router.get('/v1/quote/:symbol', async (req, res) => {
 
     console.log('/v1/quote/' + symbol)
 
-    // shellHandler('/scripts/iex/iex-quote.py', ['-s', symbol], res)
-    shellHandler('/scripts/yahoo/yahoo-quote.py', ['-s', symbol], res)
+    // utils.shellHandler('/scripts/iex/iex-quote.py', ['-s', symbol], res)
+    utils.shellHandler('/scripts/yahoo/yahoo-quote.py', ['-s', symbol], res)
 })
 
 router.get('/v1/historical/:symbol', async (req, res) => {
@@ -39,8 +36,8 @@ router.get('/v1/historical/:symbol', async (req, res) => {
 
     console.log(`/v1/historical/${symbol} (${provider})`)
 
-    // shellHandler('/scripts/yahoo/yahoo-historical.py', ['-s', symbol], res)
-    shellHandler('/scripts/py-historical/historical.py', ['-s', symbol, '-p', provider], res)
+    // utils.shellHandler('/scripts/yahoo/yahoo-historical.py', ['-s', symbol], res)
+    utils.shellHandler('/scripts/py-historical/historical.py', ['-s', symbol, '-p', provider], res)
 })
 router.get('/v1/historical/ath/:symbol', async (req, res) => {
     const symbol = req.params.symbol
@@ -48,7 +45,7 @@ router.get('/v1/historical/ath/:symbol', async (req, res) => {
 
     console.log(`/v1/ath/${symbol} (${provider})`)
 
-    shellHandler('/scripts/py-historical/historical.py', ['-s', symbol, '-p', provider, '--ath'], res)
+    utils.shellHandler('/scripts/py-historical/historical.py', ['-s', symbol, '-p', provider, '--ath'], res)
 })
 router.get('/v1/historical/dip/:symbol-:dip', async (req, res) => {
     const symbol = req.params.symbol
@@ -57,22 +54,7 @@ router.get('/v1/historical/dip/:symbol-:dip', async (req, res) => {
 
     console.log(`/v1/dip/${symbol}-${dip} (${provider})`)
 
-    shellHandler('/scripts/py-historical/historical.py', ['-s', symbol, '-p', provider, '--dip', dip], res)
+    utils.shellHandler('/scripts/py-historical/historical.py', ['-s', symbol, '-p', provider, '--dip', dip], res)
 })
-
-const shellHandler = (script, args, res) => {
-    const options = { args: args, mode: 'json' }
-
-    PythonShell.run(script, options, (error, results) => {
-        if (error) {
-            res.json({ error: error })
-            console.error(`error: %j`, error)
-        }
-        else {
-            res.json(results)
-            console.log(`results: %j`, results)
-        }
-    })
-}
 
 module.exports = router
