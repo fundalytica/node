23fbf5da59d81146a8a8e6e8afb746b0d3da0728
fotios <fotios@fundalytica.com>
2021-05-24 15:49:47 +0000

    support for multiple ids, add populate dropdown method

--- public/javascripts/modules/UI/UIUtils.js
+++ public/javascripts/modules/UI/UIUtils.js
@@ -1,14 +1,35 @@
 export default class UIUtils {
-    static show(id) {
-        UIUtils.hide(id, false)
+    static show(ids) {
+        UIUtils.hide(ids, false)
     }
 
-    static hide(id, hide = true) {
-        if(hide) {
+    static hide(ids, hide = true) {
+        if (Array.isArray(ids)) {
+            for (const id of ids) {
+                UIUtils.hideId(id, hide)
+            }
+        }
+        else {
+            const id = ids
+            UIUtils.hideId(id, hide)
+        }
+    }
+
+    static hideId(id, hide) {
+        if (hide) {
             $(`${id}`).addClass('d-none')
         }
         else {
             $(`${id}`).removeClass('d-none')
+        }
+    }
+
+    static populateDropdown(dropdown, values) {
+        $(dropdown).empty()
+
+        for (const value of values) {
+            const button = `<li><button class='dropdown-item' type='button'>${value}</button></li>`
+            $(dropdown).append(button)
         }
     }
 }
