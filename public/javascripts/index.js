fbbc0837dd863b5aa8a660fde320e7a4261ee018
fotios <fotios@fundalytica.com>
2021-05-25 09:10:14 +0000

    no jquery: ready, show

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,11 +1,14 @@
+import UIUtils from './modules/UI/UIUtils.js'
+
 import Subscription from './modules/Subscription.js'
 
 const run = () => {
-    $('.topic').show()
+    UIUtils.show('.topic')
 
     const subscription = new Subscription('#indexjs', '#welcome')
-    // subscription.hide()
+
     subscription.success()
+    // subscription.hide()
 
     // if(! $('#welcome').is(":hidden")) {
     //     typewrite('Do not miss any updates. Join now!', 'join')
@@ -15,10 +18,10 @@ const run = () => {
 const typewrite = (text, id) => {
     const textElement = document.getElementById(id)
 
-    if(textElement) {
+    if (textElement) {
         const typewriter = new Typewriter(textElement, { delay: 0.05 * 1000, cursor: null })
         typewriter.typeString(text).start()
     }
 }
 
-$(run())
+UIUtils.ready(run)
