4b5a247fdab861724be505817a5776681d5feeed
fotios <fotios@fundalytica.com>
2021-05-25 09:15:14 +0000

    no jquery refactoring

--- public/javascripts/modules/Subscription.js
+++ public/javascripts/modules/Subscription.js
@@ -1,28 +1,35 @@
+import UIUtils from './UI/UIUtils.js'
+import UIAnimationUtils from './UI/UIAnimationUtils.js'
+
 import Storage from './Storage.js'
 
 export default class Subscription {
-    constructor(script, element) {
-        this.subscription = $(script).attr('data-subscription')
-        this.subscribed_key = $(script).attr('data-subscribed-key')
-        this.element = element
+    constructor(scriptId, elementId) {
+        this.subscription = UIUtils.scriptAttribute(scriptId, 'data-subscription')
+        this.subscribed_key = UIUtils.scriptAttribute(scriptId, 'data-subscribed-key')
+
+        this.elementId = elementId
     }
 
     isSubscribed() {
         return (Storage.get(this.subscribed_key) !== undefined)
     }
 
-    hide() {
-        if(this.isSubscribed()) {
-            $(this.element).hide()
+    success() {
+        if (!UIUtils.isHidden(this.elementId)) {
+            if (this.subscription == 'success') {
+                Storage.set(this.subscribed_key, Storage.DEFAULT_VALUE)
+
+                setTimeout(() => {
+                    UIAnimationUtils.slideUp(document.querySelector(this.elementId), 1000 * 3)
+                }, 1000 * 2)
+            }
         }
     }
 
-    success() {
-        if(! $(this.element).is(":hidden")) {
-            if(this.subscription == 'success') {
-                Storage.set(this.subscribed_key, Storage.DEFAULT_VALUE)
-                $(this.element).delay(1000 * 2).slideUp(1000 * 3)
-            }
+    hide() {
+        if (this.isSubscribed()) {
+            UIUtils.hide(this.elementId)
         }
     }
 }
