08bfb7e63a1667f344e6e029f70842f582741c5e
fotios <fotios@fundalytica.com>
2021-05-31 02:16:49 +0000

    test

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,7 +1,17 @@
 import UIUtils from './modules/UI/UIUtils.js'
+import UITextUtils from './modules/UI/UITextUtils.js'
 
 const run = () => {
     console.log('hello')
+
+    let count = 1
+
+    const blinkIt = () => {
+        UITextUtils.blinkText('p', count.toString())
+        count++
+    }
+
+    setInterval(blinkIt, 100)
 }
 
 UIUtils.ready(run)
