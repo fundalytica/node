cefe04b38c0636635e98592486a3b2327099a955
fotios <fotios@fundalytica.com>
2021-06-04 15:32:15 +0000

    login text and name toggle

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -5,18 +5,20 @@ const run = () => {
     const toggleLink = () => {
         document.querySelector('#show-signup').classList.toggle('d-none')
         document.querySelector('#show-login').classList.toggle('d-none')
+        document.querySelector('#name').classList.toggle('d-none')
     }
 
     UIUtils.addListener('#show-signup', 'click', () => {
         toggleLink()
-
         UITextUtils.text('#submit', 'Sign Up')
+        UITextUtils.text('#header', 'Please Sign Up')
         document.querySelector('#form').setAttribute("action", "/signup")
     })
 
     UIUtils.addListener('#show-login', 'click', () => {
         toggleLink()
         UITextUtils.text('#submit', 'Log In')
+        UITextUtils.text('#header', 'Please Log In')
         document.querySelector('#form').setAttribute("action", "/login")
     })
 }
