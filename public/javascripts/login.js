55dcd1f7d34f90bf9c9b76d8940ea1182531f5c9
fotios <fotios@fundalytica.com>
2021-06-04 09:09:16 +0000

    login screen toggle

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,7 +1,24 @@
 import UIUtils from './modules/UI/UIUtils.js'
+import UITextUtils from './modules/UI/UITextUtils.js'
 
 const run = () => {
-    // console.log('login')
+    const toggleLink = () => {
+        document.querySelector('#show-signup').classList.toggle('d-none')
+        document.querySelector('#show-login').classList.toggle('d-none')
+    }
+
+    UIUtils.addListener('#show-signup', 'click', () => {
+        toggleLink()
+
+        UITextUtils.text('#submit', 'Sign Up')
+        document.querySelector('#form').setAttribute("action", "/signup")
+    })
+
+    UIUtils.addListener('#show-login', 'click', () => {
+        toggleLink()
+        UITextUtils.text('#submit', 'Log In')
+        document.querySelector('#form').setAttribute("action", "/login")
+    })
 }
 
 UIUtils.ready(run)
