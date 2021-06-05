e22b2890708385d000a05cdbc8ac1a4f98b27fde
fotios <fotios@fundalytica.com>
2021-06-05 10:50:05 +0000

    added name to user schema

--- models/user.js
+++ models/user.js
@@ -4,21 +4,28 @@ const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    email:      { type: String, required: true, unique: true },
+    password:   { type: String, required: true },
+    name:       { type: String, required: true }
 })
 
+// no ES6 arrow function
 UserSchema.pre('save', async function (next) {
     const user = this
+
     const saltRounds = 10
-    const hash = await bcrypt.hash(this.password, saltRounds)
+    const hash = await bcrypt.hash(user.password, saltRounds)
     this.password = hash
+
     next()
 })
 
+// no ES6 arrow function
 UserSchema.methods.isValidPassword = async function (password) {
     const user = this
+
     const compare = await bcrypt.compare(password, user.password)
+
     return compare
 }
 
