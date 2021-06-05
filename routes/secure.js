1912398b31d974332944142d41b7ea233761fb12
fotios <fotios@fundalytica.com>
2021-06-05 10:51:38 +0000

    minor

--- routes/secure.js
+++ routes/secure.js
@@ -6,11 +6,16 @@ const passport = require('passport')
 const authorizedOnly = passport.authenticate('jwt', { session: false })
 
 router.get('/profile', authorizedOnly, (req, res, next) => {
-    res.json({
-        message: 'You made it to the secure route',
+    // console.log(process.env.JWT_TOKEN_QUERY_PARAMETER)
+    // console.log(req.cookies.token)
+    const response = {
+        message: 'You made it to the secure route'
+        // token: req.cookies.token
         // user: req.user,
-        token: req.query[process.env.JWT_TOKEN_QUERY_PARAMETER]
-    })
+        // token: req.query[process.env.JWT_TOKEN_QUERY_PARAMETER]
+    }
+
+    res.json(response)
 })
 
 module.exports = router
