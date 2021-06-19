ca43171f2d2ef000df5c7b18d853a8b3d7eb8bc3
Fotios Dotcom <fotios@fundalytica.com>
2021-06-19 13:15:35 +0700

    minor change

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const fs = require('fs')
 const dotenv = require('dotenv')
 dotenv.config()
 // output env
-const env = dotenv.parse(fs.readFileSync('.env'))
-for(const key in env) {
+for(const key in dotenv.parse(fs.readFileSync('.env'))) {
     console.log(`${key}: ${process.env[key]}`)
 }
 
