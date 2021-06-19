aa2aaaa05e957a927e0e0df13263998d70764421
Fotios Dotcom <fotios@fundalytica.com>
2021-06-19 12:45:26 +0700

    show dotenv

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-require('dotenv').config()
-
 const fs = require('fs')
+
+const dotenv = require('dotenv')
+dotenv.config()
+// output env
+const env = dotenv.parse(fs.readFileSync('.env'))
+for(const key in env) {
+    console.log(`${key}: ${process.env[key]}`)
+}
 
 const express = require('express')
 const path = require('path')
