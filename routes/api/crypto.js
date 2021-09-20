e147433f66f57268b5306de2878d46a1fc818310
Fotios Dotcom <fotios@fundalytica.com>
2021-09-20 15:16:10 +0700

    added more crypto script calls

--- routes/api/crypto.js
+++ routes/api/crypto.js
@@ -3,10 +3,14 @@ const colors = require('colors')
 
 const utils = require('../utils.js')
 
+router.get(`${process.env.API_PATH}/v1/crypto/portfolio`, async (req, res) => {
+    console.log(`path: ${req.path}`.cyan)
+    utils.shellHandler(`${process.env.SCRIPTS_PATH}/crypto/portfolio.py`, ['-db', `fundalytica_${process.env.NODE_ENV}`], res)
+})
+
 router.get(`${process.env.API_PATH}/v1/crypto/futures/tickers/symbols/kraken`, async (req, res) => {
     console.log(`path: ${req.path}`.cyan)
-
-    utils.shellHandler(`${process.env.SCRIPTS_PATH}/futures/futures.py`, ['-p', 'kraken', '--tickers', '--symbols'], res)
+    utils.shellHandler(`${process.env.SCRIPTS_PATH}/crypto/futures.py`, ['-p', 'kraken', '--tickers', '--symbols'], res)
 })
 
 module.exports = router
