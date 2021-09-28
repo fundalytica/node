229b5d432817be75d62fa1f5bab52b8bab898a9b
Fotios Dotcom <fotios@fundalytica.com>
2021-09-28 22:18:39 +0700

    minor

--- routes/secure.js
+++ routes/secure.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('passport')
-
 const authorizedOnly = passport.authenticate('jwt', { session: false })
 
 router.get('/profile', authorizedOnly, (req, res, next) => {
