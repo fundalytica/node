2b37f43203ed68a058db569276b858a3c2d27c99
Fotios Dotcom <fotios@fundalytica.com>
2021-09-30 09:20:15 +0700

    rename historical

--- routes/api/historical.js
+++ routes/api/historical.js
@@ -11,7 +11,7 @@ router.get(`${process.env.API_PATH}/v1/historical/:symbol`, async (req, res) => 
 
     // utils.shellHandler(`${process.env.SCRIPTS_PATH}/yahoo/yahoo-historical.py`, ['-s', symbol], res)
     const callback = json => res.json(json)
-    utils.shellHandler(`${process.env.SCRIPTS_PATH}/py-historical/historical.py`, ['-s', symbol, '-p', provider], callback)
+    utils.shellHandler(`${process.env.SCRIPTS_PATH}/historical/historical.py`, ['-s', symbol, '-p', provider], callback)
 })
 
 router.get(`${process.env.API_PATH}/v1/historical/ath/:symbol`, async (req, res) => {
@@ -21,7 +21,7 @@ router.get(`${process.env.API_PATH}/v1/historical/ath/:symbol`, async (req, res)
     console.log(`/v1/ath/${symbol} (${provider})`)
 
     const callback = json => res.json(json)
-    utils.shellHandler(`${process.env.SCRIPTS_PATH}/py-historical/historical.py`, ['-s', symbol, '-p', provider, '--ath'], callback)
+    utils.shellHandler(`${process.env.SCRIPTS_PATH}/historical/historical.py`, ['-s', symbol, '-p', provider, '--ath'], callback)
 })
 
 router.get(`${process.env.API_PATH}/v1/historical/dip/:symbol-:dip`, async (req, res) => {
@@ -32,7 +32,7 @@ router.get(`${process.env.API_PATH}/v1/historical/dip/:symbol-:dip`, async (req,
     console.log(`/v1/dip/${symbol}-${dip} (${provider})`)
 
     const callback = json => res.json(json)
-    utils.shellHandler(`${process.env.SCRIPTS_PATH}/py-historical/historical.py`, ['-s', symbol, '-p', provider, '--dip', dip], callback)
+    utils.shellHandler(`${process.env.SCRIPTS_PATH}/historical/historical.py`, ['-s', symbol, '-p', provider, '--dip', dip], callback)
 })
 
 module.exports = router
