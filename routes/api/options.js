6100e014fd34d41404aaa8ed4b0e59ffc0c479ef
Fotios Dotcom <fotios@fundalytica.com>
2021-09-30 23:40:09 +0700

    pass data directory to ib-summary script

--- routes/api/options.js
+++ routes/api/options.js
@@ -5,9 +5,10 @@ const utils = require('../utils.js')
 
 router.get(`${process.env.API_PATH}/v1/options/portfolio`, async (req, res) => {
     console.log(`path: ${req.path}`.cyan)
+    console.log(`data: ${process.env.IB_DATA}`.green)
 
     const callback = json => res.json(json)
-    utils.shellHandler(`${process.env.SCRIPTS_PATH}/ib-summary/ib-summary.py`, ['--options', '--json'], callback)
+    utils.shellHandler(`${process.env.SCRIPTS_PATH}/ib-summary/ib-summary.py`, ['-data', process.env.IB_DATA, '--options', '--json'], callback)
 })
 
 module.exports = router
