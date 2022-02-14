76bfe3351abf638b46e5f71b5331346aba60bbf8
Fotios Dotcom <60332433+fotiosdotcom@users.noreply.github.com>
2022-02-14 16:14:35 +0700

    symbol entry in dip

--- public/javascripts/dip.js
+++ public/javascripts/dip.js
@@ -11,12 +11,31 @@ const chart = new Chart('chart')
 const UI = new UIManager('#spinner', '#error')
 
 const run = () => {
-    const options = { symbol: 'SNAP', minimumDip: 5, defaultDip: 10 }
-    const url = `${window.api_origin}/v1/historical/dip/${options.symbol}-${options.minimumDip}`
+    fetch({ symbol: 'SNAP', dip: 10 })
+
+    const form = document.querySelector('#symbol-form')
+
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+        const symbol = document.querySelector('#symbol-input').value.toUpperCase()
+        const dip = document.querySelector('#slider').value
+        fetch({ symbol: symbol, dip: dip })
+    })
+}
+
+const fetch = options => {
+    const minimumDip = 5
+    const url = `${window.api_origin}/v1/historical/dip/${options.symbol}-${minimumDip}`
+    console.log(url)
 
     UI.loading()
+    chart.show(false)
+    UIUtils.hide('#symbol-form')
+    UIUtils.hide('#slider')
+    UIUtils.hide('#dates')
+
     UITextUtils.text('#title', `${options.symbol} - Dips`)
-    UITextUtils.text('#subtitle', `-${options.defaultDip}% or worse`)
+    UITextUtils.text('#subtitle', `-${options.dip}% or worse`)
 
     const fail = error => UI.error(error)
     const done = data => {
@@ -25,26 +44,40 @@ const run = () => {
         if (data.error) {
             UITextUtils.text('#error', `${options.symbol} market data is currently not available`)
             UIUtils.show('#error')
+
+            UIUtils.hide('#title')
+            UIUtils.hide('#subtitle')
+
+            UIUtils.show('#symbol-form')
         }
         else {
             UITextUtils.text('#dates', StringUtils.range(data.dates.from, data.dates.to, "D MMM 'YY"))
 
-            // chart.addSeries(data.all.close)
-            chart.addATHSeries(data.ath.close)
-            chart.addDipSeries(data.dip.close, data.dip.dip)
+            chart.addATHSeries(data.ath.close, true)
+            const dip = -(document.querySelector('#slider').value / 100)
+            chart.addDipSeries(data.dip.close, data.dip.dip, true)
+            chart.updateDipPercentage(data.dip.close, data.dip.dip, dip)
             chart.show()
 
             const sliderInputChanged = event => {
                 const value = event.target.value
-                const percentage = -(value / 100)
                 UITextUtils.text('#subtitle', `-${value}% or worse`)
-                chart.updateDipPercentage(data.dip.close, data.dip.dip, percentage)
+
+                const dip = -(value / 100)
+                console.log(dip)
+
+                chart.updateDipPercentage(data.dip.close, data.dip.dip, dip)
             }
 
-            document.querySelector('#slider').value = options.defaultDip
+            document.querySelector('#slider').value = options.dip
             UIUtils.addListener('#slider', "change", sliderInputChanged)
 
+            UIUtils.show('#symbol-form')
             UIUtils.show("#slider")
+            UIUtils.show("#dates")
+
+            UIUtils.show('#title')
+            UIUtils.show('#subtitle')
         }
     }
 
